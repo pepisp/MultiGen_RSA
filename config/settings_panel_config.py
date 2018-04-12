de05bf1e6a43a456a79a599981e22c2f9dffdc68
Piotr Piotrowski <piotr.piotrowski@pwr.edu.pl>
2018-04-12 14:39:32 +0200

    Gotowa karta z ustawieniami

---
+++ config/settings_panel_config.py
@@ -0,0 +1,43 @@
+Tempsettings="""[{{
+      "type": "numeric",
+      "title": "Częstotliwość środkowa",
+      "desc": "Częstotliwość środkowa dla generator {1}",
+      "section": "{0}",
+      "key": "{0}Center"
+  }},
+  {{
+      "type": "numeric",
+      "title": "Span",
+      "desc": "Szerokość pasma dla generator {1}",
+      "section": "{0}",
+      "key": "{0}Span"
+  }},
+  {{
+      "type": "numeric",
+      "title": "RBW",
+      "desc": "RBW dla generator {1}",
+      "section": "{0}",
+      "key": "{0}rbw"
+  }},
+  {{
+      "type": "numeric",
+      "title": "Reference Level",
+      "desc":"Ustawienie poiomu odnisienia dla generatora {1}",
+      "section": "{0}",
+      "key": "{0}reflevel"
+  }},
+  {{
+      "type": "numeric",
+      "title": "Trace Length",
+      "desc":"Liczba próbek dla generatora {1}",
+      "section": "{0}",
+      "key": "{0}tracelen"
+  }},
+  {{
+  "type":"bool",
+  "title": "Aktywny",
+  "desc":"Wł/Wył pomiary dla generatora {1}",
+  "section": "{0}",
+  "key": "{0}enable"
+}}
+]"""
