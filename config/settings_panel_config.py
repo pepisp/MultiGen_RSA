b37ac6ca4a16a799b65a0e9d470f696510addf2b
Piotr Piotrowski <piotr.piotrowski@pwr.edu.pl>
2018-04-13 06:48:27 +0200

    Porawiona domyślna wartośc elementów boolean z true na 1

--- config/settings_panel_config.py
+++ config/settings_panel_config.py
@@ -1,43 +1,46 @@
-Tempsettings="""[{{
+#Template for settings panel. Defines all fields in each settings panel
+
+
+settingsTemplate="""[{{
       "type": "numeric",
       "title": "Częstotliwość środkowa",
-      "desc": "Częstotliwość środkowa dla generator {1}",
-      "section": "{0}",
-      "key": "{0}Center"
+      "desc": "Częstotliwość środkowa dla generator nr {1}",
+      "section": "F{0}",
+      "key": "F{0}Center"
   }},
   {{
       "type": "numeric",
       "title": "Span",
-      "desc": "Szerokość pasma dla generator {1}",
-      "section": "{0}",
-      "key": "{0}Span"
+      "desc": "Szerokość pasma dla generator nr {1}",
+      "section": "F{0}",
+      "key": "F{0}Span"
   }},
   {{
       "type": "numeric",
       "title": "RBW",
-      "desc": "RBW dla generator {1}",
-      "section": "{0}",
-      "key": "{0}rbw"
+      "desc": "RBW dla generator nr {1}",
+      "section": "F{0}",
+      "key": "F{0}rbw"
   }},
   {{
       "type": "numeric",
       "title": "Reference Level",
-      "desc":"Ustawienie poiomu odnisienia dla generatora {1}",
-      "section": "{0}",
-      "key": "{0}reflevel"
+      "desc":"Ustawienie poiomu odnisienia dla generatora nr {1}",
+      "section": "F{0}",
+      "key": "F{0}reflevel"
   }},
   {{
       "type": "numeric",
       "title": "Trace Length",
-      "desc":"Liczba próbek dla generatora {1}",
-      "section": "{0}",
-      "key": "{0}tracelen"
+      "desc":"Liczba próbek dla generatora nr {1}",
+      "section": "F{0}",
+      "key": "F{0}tracelen"
   }},
   {{
   "type":"bool",
   "title": "Aktywny",
-  "desc":"Wł/Wył pomiary dla generatora {1}",
-  "section": "{0}",
-  "key": "{0}enable"
+  "desc":"Wł/Wył pomiary dla generatora nr {1}",
+  "section": "F{0}",
+  "key": "F{0}enable"
 }}
 ]"""
--- multigen.py
+++ multigen.py
@@ -5,9 +5,9 @@ from kivy.uix.settings import SettingsWithSidebar
 from kivy.lang import Builder
 from kivy.app import App
 
-from config.settings_panel_config import Tempsettings
+from config.settings_panel_config import settingsTemplate
 
-
+FREQ_RANGE_NUM=10
 class Multigen(BoxLayout):
     # def __init__(self,**kwargs):
     #     super(Multigen,self).__init__(**kwargs)
@@ -33,22 +33,23 @@ class MultigenApp(App):
 
     def build_config(self,config):
         config.read('config\multigen.ini')
-        config.setdefaults('F1',{'f1enable':True,'f1center':169000000,'f1span':5000000,'f1rbw':100000,'f1reflevel':5000000,'f1tracelen':4096})
-        config.setdefaults('F2',{'f2enable':True,'f2center':169000000,'f2span':5000000,'f2rbw':100000,'f2reflevel':5000000,'f2tracelen':4096})
-        config.setdefaults('F3',{'f3enable':True,'f3center':169000000,'f3span':5000000,'f3rbw':100000,'f3reflevel':5000000,'f3tracelen':4096})
-        config.setdefaults('F4',{'f4enable':True,'f4center':169000000,'f4span':5000000,'f4rbw':100000,'f4reflevel':5000000,'f4tracelen':4096})
-        config.setdefaults('F5',{'f5enable':True,'f5center':169000000,'f5span':5000000,'f5rbw':100000,'f5reflevel':5000000,'f5tracelen':4096})
-        config.setdefaults('F6',{'f6enable':True,'f6center':169000000,'f6span':5000000,'f6rbw':100000,'f6reflevel':5000000,'f6tracelen':4096})
-        config.setdefaults('F7',{'f7enable':True,'f7center':169000000,'f7span':5000000,'f7rbw':100000,'f7reflevel':5000000,'f7tracelen':4096})
-        config.setdefaults('F8',{'f8enable':True,'f8center':169000000,'f8span':5000000,'f8rbw':100000,'f8reflevel':5000000,'f8tracelen':4096})
-        config.setdefaults('F9',{'f9enable':True,'f9center':169000000,'f9span':5000000,'f9rbw':100000,'f9reflevel':5000000,'f9tracelen':4096})
+        config.setdefaults('F1',{'f1enable':1,'f1center':169000000,'f1span':5000000,'f1rbw':100000,'f1reflevel':5000000,'f1tracelen':4096})
+        config.setdefaults('F2',{'f2enable':1,'f2center':169000000,'f2span':5000000,'f2rbw':100000,'f2reflevel':5000000,'f2tracelen':4096})
+        config.setdefaults('F3',{'f3enable':1,'f3center':169000000,'f3span':5000000,'f3rbw':100000,'f3reflevel':5000000,'f3tracelen':4096})
+        config.setdefaults('F4',{'f4enable':1,'f4center':169000000,'f4span':5000000,'f4rbw':100000,'f4reflevel':5000000,'f4tracelen':4096})
+        config.setdefaults('F5',{'f5enable':1,'f5center':169000000,'f5span':5000000,'f5rbw':100000,'f5reflevel':5000000,'f5tracelen':4096})
+        config.setdefaults('F6',{'f6enable':1,'f6center':169000000,'f6span':5000000,'f6rbw':100000,'f6reflevel':5000000,'f6tracelen':4096})
+        config.setdefaults('F7',{'f7enable':1,'f7center':169000000,'f7span':5000000,'f7rbw':100000,'f7reflevel':5000000,'f7tracelen':4096})
+        config.setdefaults('F8',{'f8enable':1,'f8center':169000000,'f8span':5000000,'f8rbw':100000,'f8reflevel':5000000,'f8tracelen':4096})
+        config.setdefaults('F9',{'f9enable':1,'f9center':169000000,'f9span':5000000,'f9rbw':100000,'f9reflevel':5000000,'f9tracelen':4096})
+
     def build_settings(self,settings):
         # with open('config\settings.json',encoding='utf8') as settingsfile:
         #     settings.add_json_panel(u"Ustawienia dla generatora 1",self.config,data=settingsfile.read())
         #print(Tempsettings.format('1','1'))
         #settings.add_json_panel(u"Ustawienia dla generatora 1",self.config,data=f1settings)
-        for gen_no in range(1,10):
-            settings.add_json_panel(u"Generator {}".format(str(gen_no)),self.config,data=Tempsettings.format('F'+str(gen_no),'nr '+str(gen_no)))
+        for gen_no in range(1,FREQ_RANGE_NUM):
+            settings.add_json_panel(u"Generator {}".format(str(gen_no)),self.config,data=settingsTemplate.format(str(gen_no),str(gen_no)))
 
 if __name__=='__main__':
     MultigenApp().run()
