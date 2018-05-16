4f316bc3adf6dadce9a36a93b343cbced50a305b
Piotr Piotrowski <piotr.piotrowski@pwr.edu.pl>
2018-05-16 12:52:35 +0200

    Zmiana w nazwach ustawien

--- config/settings_panel_config.py
+++ config/settings_panel_config.py
@@ -5,42 +5,42 @@ settingsTemplate="""[{{
       "type": "numeric",
       "title": "Częstotliwość środkowa",
       "desc": "Częstotliwość środkowa dla zakresu nr {1}",
-      "section": "F{0}",
-      "key": "F{0}Center"
+      "section": "Range{0}",
+      "key": "centerFreq"
   }},
   {{
       "type": "numeric",
       "title": "Span",
       "desc": "Szerokość pasma dla zakresu nr {1}",
-      "section": "F{0}",
-      "key": "F{0}Span"
+      "section": "Range{0}",
+      "key": "span"
   }},
   {{
       "type": "numeric",
       "title": "RBW",
       "desc": "RBW dla zakresu nr {1}",
-      "section": "F{0}",
-      "key": "F{0}rbw"
+      "section": "Range{0}",
+      "key": "rbw"
   }},
   {{
       "type": "numeric",
       "title": "Reference Level",
       "desc":"Ustawienie poiomu odnisienia dla zakresu nr {1}",
-      "section": "F{0}",
-      "key": "F{0}reflevel"
+      "section": "Range{0}",
+      "key": "reflevel"
   }},
   {{
       "type": "numeric",
       "title": "Trace Length",
       "desc":"Liczba próbek dla zakresu nr {1}",
-      "section": "F{0}",
-      "key": "F{0}tracelen"
+      "section": "Range{0}",
+      "key": "tracelen"
   }},
   {{
   "type":"bool",
   "title": "Aktywny",
   "desc":"Wł/Wył pomiary dla zakresu nr {1}",
-  "section": "F{0}",
-  "key": "F{0}enable"
+  "section": "Range{0}",
+  "key": "enable"
 }}
 ]"""
--- multigen.py
+++ multigen.py
@@ -126,22 +126,22 @@ class MultigenApp(App):
         self.title="Multigen"
         self.settings_cls=SettingsWithSidebar
 
-        #multigen.ids.Gen1Button.text=self.config.get("F1","F1Center")
+        #multigen.ids.Gen1Button.text=self.config.get("F1","centerFreq")
         self.use_kivy_settings=False
 
         return multigen
 
     def build_config(self,config):
         config.read('config\multigen.ini')
-        config.setdefaults('F1',{'f1enable':1,'f1center':169000000,'f1span':5000000,'f1rbw':100000,'f1reflevel':5000000,'f1tracelen':4096})
-        config.setdefaults('F2',{'f2enable':1,'f2center':169000000,'f2span':5000000,'f2rbw':100000,'f2reflevel':5000000,'f2tracelen':4096})
-        config.setdefaults('F3',{'f3enable':1,'f3center':169000000,'f3span':5000000,'f3rbw':100000,'f3reflevel':5000000,'f3tracelen':4096})
-        config.setdefaults('F4',{'f4enable':1,'f4center':169000000,'f4span':5000000,'f4rbw':100000,'f4reflevel':5000000,'f4tracelen':4096})
-        config.setdefaults('F5',{'f5enable':1,'f5center':169000000,'f5span':5000000,'f5rbw':100000,'f5reflevel':5000000,'f5tracelen':4096})
-        config.setdefaults('F6',{'f6enable':1,'f6center':169000000,'f6span':5000000,'f6rbw':100000,'f6reflevel':5000000,'f6tracelen':4096})
-        config.setdefaults('F7',{'f7enable':1,'f7center':169000000,'f7span':5000000,'f7rbw':100000,'f7reflevel':5000000,'f7tracelen':4096})
-        config.setdefaults('F8',{'f8enable':1,'f8center':169000000,'f8span':5000000,'f8rbw':100000,'f8reflevel':5000000,'f8tracelen':4096})
-        config.setdefaults('F9',{'f9enable':1,'f9center':169000000,'f9span':5000000,'f9rbw':100000,'f9reflevel':5000000,'f9tracelen':4096})
+        config.setdefaults('Range1',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range2',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range3',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range4',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range5',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range6',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range7',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range8',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
+        config.setdefaults('Range9',{'enable':1,'centerFreq':169000000,'span':5000000,'rbw':100000,'reflevel':5000000,'tracelen':4096})
 
     def build_settings(self,settings):
 
